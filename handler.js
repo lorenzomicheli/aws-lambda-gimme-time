d81296eebc8be8a8c14bf9e991c65e1aba51a341
Lorenzo Micheli <lorenzo.micheli@tipico.com>
2018-10-21 23:27:54 +0200

    linting fixes

--- handler.js
+++ handler.js
@@ -1,12 +1,10 @@
-'use strict';
-
 const time = require('./time');
 
 module.exports.gimmetime = async () => {
-    const currentTime = time.getTime();
-    currentTime.setTimezone("Europe/Rome");
-    return {
-        statusCode: '200',
-        body: 'The time in Rome is: ' + currentTime.toString(),
-    };
+  const currentTime = time.getTime();
+  currentTime.setTimezone('Europe/Rome');
+  return {
+    statusCode: '200',
+    body: `The time in Rome is: ${currentTime.toString()}`
+  };
 };
